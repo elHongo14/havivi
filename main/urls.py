9086bc8f632a27b7f5596ca1afeacf7ea8d2a538
Luis Hernández Martínez <elhongo1409@outlook.com>
2025-02-15 00:05:07 -0600

    login animation

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name='main'
+
 urlpatterns = [
     path('', views.home_view, name="home"),
     path('login/', views.login_view, name="login"),
