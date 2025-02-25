753fdfcfb89c54fe2275fa9a524c742dd80d8cd3
Luis Hernández Martínez <elhongo1409@outlook.com>
2025-02-24 22:10:18 -0600

    Tabla galería añadida a la vista de administrador Django

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import (
     Prueba,
+    Galeria,
 )
 
 # Register your models here.
@@ -9,4 +10,9 @@ class PruebaAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'descripción', 'fecha_creacion', 'fecha_actualizacion',)
     search_fields = ('nombre',)
 
+class GaleriaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'descripción', 'img_ubi', 'foto_bool', 'link',)
+    search_fields = ('nombre', 'img_ubi')
+
 admin.site.register(Prueba, PruebaAdmin)
+admin.site.register(Galeria, GaleriaAdmin)
