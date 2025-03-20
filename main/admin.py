248f73e11ff9f195d51114c181539ecc7e74e718
Franky-uts <107886319+Franky-uts@users.noreply.github.com>
2025-03-19 23:06:46 -0600

    Cambios ligeros en admin
    realmente ligeros

--- main/admin.py
+++ main/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import (
     Prueba,
     Galeria,
+    Solicitudes,
+    Empleos,
+    Solicitud_Empleo,
 )
 
 # Register your models here.
@@ -14,5 +17,20 @@ class GaleriaAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'descripción', 'img_ubi', 'foto_bool', 'link',)
     search_fields = ('nombre', 'img_ubi')
 
+class SolicitudesAdmin(admin.ModelAdmin):
+    list_display = ('tipo','fecha','nombre','descripción','objetivo','archivo')
+    search_fields = ('nombre','fecha','tipo')
+
+class EmpleosAdmin(admin.ModelAdmin):
+    list_display = ('titulo','área','descripción','requisitos','horario','paga',)
+    search_fields = ('titulo','área',)
+
+class Solicitud_EmpleoAdmin(admin.ModelAdmin):
+    list_display = ('archivo','empleo',)
+    search_fields = ('empleo',)
+
 admin.site.register(Prueba, PruebaAdmin)
 admin.site.register(Galeria, GaleriaAdmin)
+admin.site.register(Solicitudes,SolicitudesAdmin)
+admin.site.register(Empleos,EmpleosAdmin)
+admin.site.register(Solicitud_Empleo,Solicitud_EmpleoAdmin)
